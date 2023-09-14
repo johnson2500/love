8ddd0a3344f621101f1a039fe8de360d4ff7f359
Ryan Johnson <johnson2500@live.com>
2023-09-13 21:59:24 -0400

    To my lovely wife! - Ryan

--- index.py
+++ index.py
@@ -6,20 +6,21 @@ import webbrowser
 window = tk.Tk()
 
 
-def on_button_click(): 
+def on_button_click():
     tk.messagebox.Message(master=None, message="I love you so much!").show()
-    print("Button clicked!")
 
 
-greeting = tk.Label(text="Hello.")
+greeting = tk.Label(
+    text="To My Lovely Wife...", fg="white", bg="black", width=50, height=10
+)
 
 
 button2 = tk.Button(
-    text="Click me!", width=5, height=5, bg="blue", fg="Black", command=on_button_click
+    text="Click me!", width=50, height=5, bg="blue", fg="Black", command=on_button_click
 )
+greeting.pack()
 button2.pack()
 
 
-greeting.pack()
 window.mainloop()
 
